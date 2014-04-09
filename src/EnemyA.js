8bbcd2e008e73c7cd1b25b130ff445c66089cd41
melmulcahy <melmulcahy32@gmail.com>
2014-04-09 12:38:19 -0400

    Images

--- src/EnemyA.js
+++ src/EnemyA.js
@@ -2,11 +2,11 @@ var EnemyA = Class.create(Entity, {
     health:1,
     _currX:0,
     _currY:0,
-    scale:1,
+    scale:0.1,
     pSprite:null,
     initialize: function() {
             this.pSprite= new Image();
-            this.pSprite.src = './res/Normal/CloseSelected.png';
+            this.pSprite.src = './res/Normal/birdenemy.png';
             var self = this;
             this._currX = 250;
             this._currY = 300;
