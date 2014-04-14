4806dda656cb83ef57194d94bbcb7578fd18eea6
David <davidtanger@gmail.com>
2014-04-14 13:33:56 -0400

    Start Screen finished. Need to crop the image to size.

---
+++ src/startScreen.js
@@ -0,0 +1,28 @@
+var start = {
+    centerTxt:function(ctx, text, y) {
+            var measurement = ctx.measureText(text);
+            var x = (ctx.canvas.width - measurement.width) / 2;
+            ctx.fillText(text, x, y);
+    },
+    startScreen:function(){
+        canvas = document.getElementById('gameCanvas');
+        imgPath='./res/Normal/nekostart.png';
+        img = new Image();
+        img.src = imgPath;
+        img.onload=function(){
+            ctx=canvas.getContext('2d');
+            var y = ctx.canvas.height / 2;
+            title='Neko Pew Pew!';
+            ctx.fillStyle = 'white';
+            ctx.font = '48px monospace';
+            start.centerTxt(ctx, title, y);
+            x=(ctx.canvas.width - img.width) / 2;
+            ctx.drawImage(img, x, y);
+        }
+        //alert(canvas.width);
+        //alert(canvas.height);
+    }
+
+
+
+};
